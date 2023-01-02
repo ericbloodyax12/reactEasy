d49d77288f06ff682653c89034dbdf216467c332
Young Eric <erikbonopart12@gmail.com>
2023-01-02 14:37:45 +0300

    change Accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,17 @@ function App() {
 function Rating () {
   return(
   <div>
-      <div> <Star /> </div>
+      <> This is title </>
+       <Star />
+      <Star />
+      <Star />
+      <Accordion />
+
   </div>
   )}
 
 function Star() {
+    console.log("Star rendered")
     return (
         <div>
             This is star component in Rating
@@ -31,13 +37,27 @@ function Star() {
 }
 
 function Accordion () {
-    return <div>
-          <h3> Menu </h3>
-      <ul>
+    return (
+        <>
+        <AccordionTitle />
+      <AccordionBody />
+        </>
+    )}
+
+export default App;
+
+function AccordionTitle() {
+    return (
+    <h3> Menu </h3>
+    )
+};
+
+function AccordionBody() {
+    return (
+    <ul>
         <li>1</li>
         <li>2</li>
         <li>3</li>
-      </ul>
-    </div>
+    </ul>
+    )
 }
-export default App;
